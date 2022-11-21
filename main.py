2682f8465e1fd9011abbd0f7262364f9872f153b
Erace17 <73711284+Erace17@users.noreply.github.com>
2022-11-21 23:11:53 +0100

    test

--- main.py
+++ main.py
@@ -3,3 +3,15 @@
 # X | X |   
 # ----------
 #   |   |
+
+class Spielfeld():
+    
+    def __init__(self):
+        pass
+
+    def feld():
+        print("   |    |   ")
+        print("------------")
+        print("   |    |   ")
+        print("------------")
+        print("   |    |   ")
