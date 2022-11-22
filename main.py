210c175994a24d6cb7fd94e28ab19a64dc51d449
Erace17 <73711284+Erace17@users.noreply.github.com>
2022-11-23 00:15:02 +0100

    testing

--- main.py
+++ main.py
@@ -3,15 +3,33 @@
 # X | X |   
 # ----------
 #   |   |
+# Felder von 1-9 durchnummerieren
+
 
 class Spielfeld():
     
     def __init__(self):
-        pass
-
+        self.brett = (0,0,0,0,0,0,0,0,0)
+    
     def feld():
         print("   |    |   ")
         print("------------")
         print("   |    |   ")
         print("------------")
         print("   |    |   ")
+
+    def symbol():
+        pass   
+
+    def spielzug(self):
+        print("")
+        zahl = int(input("Auf welches Feld wollen sie setzen: "))
+        self.brett[zahl]
+        if self.brett[zahl] != 0:
+            print("Hier wurde bereits hingesetzt!")
+        elif self.brett[zahl] == 0:
+            self.brett[zahl] = figur
+
+
+
+
